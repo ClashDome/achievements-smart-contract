8c0dd2409540a8a679f4a3a6d6b9a16765c60a4f
Enriqueto <enrique@ravalmatic.com>
2021-06-07 17:08:40 +0200

    reading NFT mutable data

--- include/clashdomeach.hpp
+++ include/clashdomeach.hpp
@@ -1,6 +1,5 @@
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
-#include <eosio/crypto.hpp>
 #include <eosio/transaction.hpp>
 #include <atomicassets.hpp>
 #include <atomicdata.hpp>
@@ -13,32 +12,19 @@ using namespace std;
 
 #define CONTRACTN name("clashdomeach")
 
-CONTRACT clashdomeach : public contract
-{
-public:
-   using contract::contract;
+CONTRACT clashdomeach : public contract {
 
-    ACTION actiontest(
-        uint64_t param1,
-        uint64_t param2
-    );
+public:
+
+    using contract::contract;
+
+    ACTION claim(name account, uint8_t id, uint64_t asset_id);
+    ACTION logclaim(atomicassets::ATTRIBUTE_MAP  mdata);
 
 private:
 
-    TABLE test_table_s {
-        uint64_t pack_id;
-        name     collection_name;
-        uint32_t unlock_time;
-        int32_t  pack_template_id;
-
-        uint64_t primary_key() const { return pack_id; }
-        uint64_t by_template_id() const { return (uint64_t) pack_template_id; };
+    TABLE ludiorewards{
+        uint8_t id;
+        vector <uint16_t> values;
     };
-
-    typedef multi_index<name("testtable"), test_table_s,
-        indexed_by < name("templateid"), const_mem_fun < test_table_s, uint64_t, &test_table_s::by_template_id>>>
-    test_table_t;
-
-    test_table_t             test_table           = test_table_t(get_self(), get_self().value);
-
 };
--- src/clashdomeach.cpp
+++ src/clashdomeach.cpp
@@ -1,12 +1,39 @@
 #include <clashdomeach.hpp>
 
-ACTION clashdomeach::actiontest(
-    uint64_t param1,
-    uint64_t param2
-) {
-    require_auth(get_self());
+void clashdomeach::claim(name account, uint8_t id, uint64_t asset_id) {
 
-    // 
-    //
-    //
+    require_auth(account);
+
+    atomicassets::assets_t player_assets = atomicassets::get_assets(account);
+    auto asset_itr = player_assets.require_find(asset_id, "No NFT with this ID exists");
+
+    // comprobar que el asset es de la coleccion que toca
+    check(asset_itr->collection_name == name("clashdomenft"), "NFT doesn't correspond to clashdomenft");
+    check(asset_itr->schema_name == name("gamedatatest"), "NFT doesn't correspond to schema gamedata");
+    check(asset_itr->template_id == 91070, "NFT doesn't correspond to template  #91070");
+
+    atomicassets::schemas_t collection_schemas = atomicassets::get_schemas(name("clashdomenft"));
+    auto schema_itr = collection_schemas.find(name("gamedatatest").value);
+
+    vector <uint8_t> inmutable_serialized_data = asset_itr->immutable_serialized_data;
+    vector <uint8_t> mutable_serialized_data = asset_itr->mutable_serialized_data;
+
+    // TODO: deserializar estos 2 vectores
+    atomicassets::ATTRIBUTE_MAP mdata = {};
+
+    mdata = atomicdata::deserialize(mutable_serialized_data, schema_itr->format);
+
+    action(
+        permission_level{get_self(), name("active")},
+        get_self(),
+        name("logclaim"),
+        std::make_tuple (
+            mdata
+        )
+    ).send();
+}
+
+void clashdomeach::logclaim( atomicassets::ATTRIBUTE_MAP  mdata) {
+
+    require_auth(get_self());
 }
